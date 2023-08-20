2071149f18fa0094d5c1d30fcbd3364d32c1f253
jonasbeltoft <jonasb5@hotmail.dk>
2023-08-20 14:32:59 +0200

    viewer layout

--- front-end/src/app/viewer/viewer.component.ts
+++ front-end/src/app/viewer/viewer.component.ts
@@ -11,11 +11,17 @@ export class ViewerComponent {
 
   videoId: string
   videoName: string
+  seriesName: string
 
   constructor(route: ActivatedRoute, private titleService: Title) {
     route.params.subscribe((params) => {
       this.videoId = params["id"];
     });
+
+    // @TODO Fetch
+    this.videoName = "Video name"
+    this.seriesName = "Series name"
+
     this.titleService.setTitle(this.videoName ? 'Video: ' + this.videoName : 'Stream');
   }
 }
