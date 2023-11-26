9cf6b3ad637a854615bf8bb6fd1aa37a70acf3ef
jonasbeltoft <jonasb5@hotmail.dk>
2023-11-26 09:45:32 +0100

    Mobile video session (untested)

--- front-end/src/app/viewer/viewer.component.ts
+++ front-end/src/app/viewer/viewer.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { SearchService } from '../search.service';
@@ -8,7 +8,9 @@ import { SearchService } from '../search.service';
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.css']
 })
-export class ViewerComponent implements AfterViewInit {
+export class ViewerComponent implements OnInit {
+
+  @ViewChild('videoplayer') videoplayer: ElementRef;
 
   video: {
     id: string;
@@ -27,11 +29,29 @@ export class ViewerComponent implements AfterViewInit {
     });
   }
 
-  async ngAfterViewInit() {
+  async ngOnInit() {
     // @TODO Fetch
     this.video = await this.searchService.getById(this.paramId)
 
     this.titleService.setTitle(this.video && this.video?.title ? this.video.title : 'Streaming...');
+
+    if ('mediaSession' in navigator) this.createMobileSession()
+
+  }
+
+  createMobileSession() {
+    navigator.mediaSession.metadata = new MediaMetadata({
+      title: this.video.title,
+      album: this.video.series ? this.video.series : undefined,
+      artwork: [
+        { src: "assets/film-slate-partly-colored.png", sizes: "512x512", type: "image/png" },
+        { src: "assets/dev_assets/dev-poster.png", sizes: "1804x970", type: "image/png" },
+      ]
+    })
+    navigator.mediaSession.setActionHandler('play', this.videoplayer.nativeElement.play)
+    navigator.mediaSession.setActionHandler('pause', this.videoplayer.nativeElement.pause)
+    navigator.mediaSession.setActionHandler('nexttrack', this.goToNext)
+    navigator.mediaSession.setActionHandler('previoustrack', this.goToPrevious)
   }
 
   goToPrevious() {
