c3fccbd63a995ccb64b656f773430974051d0897
jonasbeltoft <jonasb5@hotmail.dk>
2023-12-09 06:55:32 +0100

    installed and applied 'editorconfig' for vscode

--- front-end/src/app/front-page/front-page.component.ts
+++ front-end/src/app/front-page/front-page.component.ts
@@ -4,30 +4,30 @@ import { SearchService } from '../search.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-front-page',
-    templateUrl: './front-page.component.html',
-    styleUrls: ['./front-page.component.css']
+	selector: 'app-front-page',
+	templateUrl: './front-page.component.html',
+	styleUrls: ['./front-page.component.css']
 })
 export class FrontPageComponent implements OnInit, OnDestroy {
 
-    prevViewed: { id: string, title: string, episode?: string, series?: string, shortDesc: string, poster: string }[] = []
+	prevViewed: { id: string, title: string, episode?: string, series?: string, shortDesc: string, poster: string }[] = []
 
-    searchSubsribtion: Subscription
+	searchSubsribtion: Subscription
 
-    constructor(private router: Router, private searchService: SearchService) { }
+	constructor(private router: Router, private searchService: SearchService) { }
 
-    ngOnInit() {
-        this.searchSubsribtion = this.searchService.searchEvent.subscribe((files) => {
-            this.prevViewed = files
-        })
-        this.searchService.getRecent(4)
-    }
+	ngOnInit() {
+		this.searchSubsribtion = this.searchService.searchEvent.subscribe((files) => {
+			this.prevViewed = files
+		})
+		this.searchService.getRecent(4)
+	}
 
-    goToVideo(id: string) {
-        this.router.navigate(['video-viewer', id])
-    }
+	goToVideo(id: string) {
+		this.router.navigate(['video-viewer', id])
+	}
 
-    ngOnDestroy() {
-        this.searchSubsribtion.unsubscribe()
-    }
+	ngOnDestroy() {
+		this.searchSubsribtion.unsubscribe()
+	}
 }
